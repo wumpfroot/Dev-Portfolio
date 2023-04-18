8b76e8d465147fb28ac9c42257255e8359213b82
wumpfroot <markuskojo@msn.com>
2023-04-18 20:51:10 +0100

    Removed divider import from app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Projects from "./components/projects/Projects";
 import About from "./components/About";
 import Skills from "./components/skills/Skills";
 import Contact from "./components/contact/Contact";
-import Divider from "./components/PageDivider/Divider";
 
 function App() {
 	return (
