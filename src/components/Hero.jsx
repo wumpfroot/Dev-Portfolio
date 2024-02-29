5fcbcf54937b39946de049bcc84ba9e868c7293b
wumpfroot <markuskojo@msn.com>
2024-02-29 15:27:03 +0000

    Changes to hero text + GH link on projects

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,56 +5,29 @@ import Divider from "./PageDivider/Divider";
 const Hero = () => {
 	return (
 		<>
-			<section
-				id="hero"
-				className="relative animate-fadeIn h-screen flex md:items-center md:justify-center px-4"
-			>
+			<section id="hero" className="relative animate-fadeIn h-screen flex md:items-center md:justify-center px-4">
 				<div className="text-xl md:text-2xl">
 					<h1 className="text-4xl my-8 md:text-8xl">
 						<h1 className="text-cyan-400">Hello, my name is</h1>
 						<Typewriter
 							onInit={(typewriter) => {
-								typewriter
-									.pasteString("your name")
-									.pauseFor(2000)
-									.changeDeleteSpeed(10)
-									.deleteChars(8)
-									.typeString("Markus Kojo")
-									.start();
+								typewriter.pasteString("your name").pauseFor(2000).changeDeleteSpeed(10).deleteChars(8).typeString("Markus Kojo").start();
 							}}
 						/>
 					</h1>
 					<div className="flex justify-evenly my-5 md:my-9 text-3xl md:text-4xl items-center">
-						<a
-							title="GitHub profile"
-							href="https://github.com/wumpfroot"
-							className="cursor-pointer"
-							target="_blank"
-							rel="noopener noreferrer"
-						>
+						<a title="GitHub profile" href="https://github.com/wumpfroot" className="cursor-pointer" target="_blank" rel="noopener noreferrer">
 							<AiOutlineGithub />
 						</a>
-						<a
-							title="LinkedIn profile"
-							href="https://www.linkedin.com/in/markus-kojo/"
-							className="cursor-pointer"
-							target="_blank"
-							rel="noopener noreferrer"
-						>
+						<a title="LinkedIn profile" href="https://www.linkedin.com/in/markus-kojo/" className="cursor-pointer" target="_blank" rel="noopener noreferrer">
 							<AiOutlineLinkedin />
 						</a>
-						<a
-							className="text-cyan-400 text-3xl"
-							href="./files/CV2023.pdf"
-							download
-						>
+						<a className="text-cyan-400 text-3xl" href="./files/CV2023.pdf" download>
 							download resume
 						</a>
 					</div>
 					<p>
-						Frontend developer, born in{" "}
-						<span className="text-blue-400">Finland</span>. Located in{" "}
-						<span className="text-red-500">Manchester</span>, UK.
+						Software developer, born in <span className="text-blue-400">Finland</span>. Located in <span className="text-red-500">Manchester</span>, UK.
 						<br /> Looking for my first job opportunity in the coding industry.
 					</p>
 					<p>Take a look at my projects and what I can offer below.</p>
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,10 +1,18 @@
 import ProjectCard from "./ProjectCard";
 import { projects } from "../projectData";
 
+import { AiOutlineGithub } from "react-icons/ai";
+
 const Projects = () => {
 	return (
 		<div id="projects" className="mx-auto max-w-xl md:min-w-full">
-			<h2 className="text-6xl pt-28 text-center">Projects</h2>
+			<h2 className="text-4xl pt-28 text-center mb-5 md:text-6xl">Selected projects</h2>
+			<i className="flex justify-center items-center gap-3 text-base md:text-lg">
+				more projects at my github{" "}
+				<a title="GitHub profile" href="https://github.com/wumpfroot" className="cursor-pointer" target="_blank" rel="noopener noreferrer">
+					<AiOutlineGithub size={50} />
+				</a>{" "}
+			</i>
 			{projects.map((project) => (
 				<div key={project.id}>
 					<ProjectCard {...project} />
